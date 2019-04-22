6c39cd70e8e0a1aa6a7eab9e653d7ec4f0fe4281
Frederico Veloso <frederico.veloso@anac.gov.br>
2019-04-22 14:06:01 -0300

    Implementa cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors');
 const multer = require('multer');
 const ffmpeg = require('fluent-ffmpeg');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended": false}));
 app.use(express.static(__dirname + '/public'));
