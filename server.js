6d12cfcaff01a3f5aae43edca40e2b3be1633d7e
Frederico Veloso <frederico.veloso@anac.gov.br>
2019-04-24 10:44:58 -0300

    Update package.json

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const ffmpeg = require('fluent-ffmpeg');
-const converter = require('converter');
 const bodyParser = require('body-parser');
 const app = express();
 
